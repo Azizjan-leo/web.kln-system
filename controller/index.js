ef5a627bbe8fa0aa31626893a2701a940a59008b
Umar <umarsabyrbekov123@gmail.com>
2025-03-16 15:38:29 +0600

    first

---
+++ controller/index.js
@@ -0,0 +1,20 @@
+
+// import {handleMessage} from './lib/telegram.js';
+// async function handler(req, method) {
+//     const {body} = req;
+//     if (body) {
+//         const messageObj = body.message;
+//         await handleMessage(messageObj);
+//     }
+//     return;
+// }
+
+// async function handler(req) {
+//     const { body } = req;
+//     if (body && body.message) {
+//         await handleMessage(body.message);
+//     }
+// }
+
+
+// module.exports = {handler}
